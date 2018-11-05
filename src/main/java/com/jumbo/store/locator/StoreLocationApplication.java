fdea15df6f2a5f14b5d73a964027b8e13390e9a2
JimmyHeller <ali.tommy.heller2009@gmail.com>
2018-11-05 09:31:13 +0330

    testing commit

--- src/main/java/com/jumbo/store/locator/StoreLocationApplication.java
+++ src/main/java/com/jumbo/store/locator/StoreLocationApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StoreLocationApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(StoreLocationApplication.class, args);
     }
