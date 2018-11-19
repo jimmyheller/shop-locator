fc1dfe910430ac2bfe87f479493d17cd210196dc
JimmyHeller <ali.tommy.heller2009@gmail.com>
2018-11-19 15:42:19 +0330

    removing debugger statement from the app.js, also changing the type of message handling the index page ,

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -2,7 +2,6 @@ var app = angular.module('store-loc', []);
 app.controller('searchByCoordinates', function ($scope, $http) {
     console.log($scope.appMessage);
     $scope.submitByCoordinates = function () {
-        debugger;
         if ($scope.latitude > 90 || $scope.latitude < -90) {
             $scope.appMessage = "please enter valid latitude";
             return;
@@ -14,10 +13,13 @@ app.controller('searchByCoordinates', function ($scope, $http) {
         var url = "/api/v1/stores/coordinates?lat=" + $scope.latitude + "&long=" + $scope.longitude;
         $http.get(url)
             .then(function (response) {
+                if (response.data.responseCode != 0) {
+                    $scope.appMessage = response.data.message;
+                }
                 $scope.data = response.data.data;
-                $scope.appMessage = '';
+                $scope.appMessage = response.data.message;
             }, function () {
-                $scope.appMessage = Response.data.message;
+                $scope.appMessage = 'There was an error in finding your request,please try again.'
             });
     }
 });
